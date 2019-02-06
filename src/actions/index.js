0bfd5b18cb173cb26cdee99e0b7372c023ff3d7a
Bao Pham <ryanvuepham@gmail.com>
2019-02-05 22:19:51 -0600

    define action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
+import axios from 'axios'
+
+export const FETCHING_CHARS = 'FETCHING_ChARS'
+export const ERROR_FETCHING_CHARS = 'ERROR_FETCHING_CHARS'
+export const CHARS_FETCHED = 'CHARS_FETCHED'
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
@@ -6,3 +12,20 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+const fetchChars = () => {
+    const starWarsChars = axios.get(`https://swapi.co/api/people`);
+    return dispatch => {
+        dispatch({ type: FETCHING_CHARS })
+        starWarsChars // is a promise and we can call .then on it
+        .then(res =>{
+            console.log(res)
+            dispatch({type: CHARS_FETCHED, payload: [] }) // dispatch another action
+            
+        }).catch(err => {
+            dispatch({type: ERROR_FETCHING_CHARS, payload: 'Error Fetching Chars'}) // explicitly send a string
+        })
+    }
+}
+
+fetchChars();
